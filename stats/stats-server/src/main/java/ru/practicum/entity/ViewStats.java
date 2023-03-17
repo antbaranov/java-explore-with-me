c7df4034a2b71e6f0ef44e2ba9fc8364c985450a
Anton <antnbrnv@yandex.ru>
2023-03-17 11:48:05 +0300

    fix: избавиться от @Data

--- stats/stats-server/src/main/java/ru/practicum/entity/ViewStats.java
+++ stats/stats-server/src/main/java/ru/practicum/entity/ViewStats.java
@@ -3,10 +3,15 @@ package ru.practicum.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ViewStats {
 
     private String app;
