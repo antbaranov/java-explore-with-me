bb049c5157d5afe5fa363566c2cb7f422ee89caf
Anton <antnbrnv@yandex.ru>
2023-03-17 17:03:20 +0300

    fix: StatsRepository разбить на два запроса

--- stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -10,14 +10,6 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-    /*@Query(value = "SELECT new ru.practicum.entity.ViewStats(e.app, e.uri, " +
-            "(CASE WHEN :unique = true then COUNT(DISTINCT e.ip) else COUNT(e.ip) end)) " +
-            "FROM EndpointHit AS e " +
-            "WHERE e.timestamp BETWEEN :start AND :end AND e.uri IN (:uris) " +
-            "GROUP BY e.app, e.uri " +
-            "ORDER BY COUNT(e.ip) DESC")
-    List<ViewStats> findStatsByDates(Timestamp start, Timestamp end, List<String> uris, boolean unique);
-*/
     @Query(value = "SELECT new ru.practicum.entity.ViewStats(e.app, e.uri, COUNT(e.ip)) " +
             "FROM EndpointHit AS e " +
             "WHERE e.timestamp BETWEEN :start AND :end AND e.uri IN (:uris) " +
