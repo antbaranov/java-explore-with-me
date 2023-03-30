2d3a40c2d31344482175dc004c518a4396171840
Anton <antnbrnv@yandex.ru>
2023-03-30 15:12:49 +0300

    fix: Stats

--- main-service/src/main/java/ru/practicum/main_service/service/event/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/main_service/service/event/EventServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.main_service.dto.event.EventFullDto;
 import ru.practicum.main_service.dto.event.EventShortDto;
@@ -28,7 +27,7 @@ import ru.practicum.main_service.mapper.EventMapper;
 import ru.practicum.main_service.repository.CategoryRepository;
 import ru.practicum.main_service.repository.EventRepository;
 import ru.practicum.main_service.repository.UserRepository;
-import ru.practicum.stats_client.StatClient;
+import ru.practicum.main_service.service.statistics.StatisticsService;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -58,8 +57,7 @@ public class EventServiceImpl implements EventService {
     private final EntityManager entityManager;
     private final String datePattern = DATE;
     private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(datePattern);
-
-    private final StatClient statClient;
+    private final StatisticsService statisticsService;
 
     @Override
     public EventFullDto createEvent(Long userId, NewEventDto newEventDto) {
@@ -331,7 +329,7 @@ public class EventServiceImpl implements EventService {
         }
 
         setView(events);
-        sendStat(events, request);
+        statisticsService.sendStat(events, request);
         return eventMapper.toEventFullDtoList(events);
     }
 
@@ -339,60 +337,12 @@ public class EventServiceImpl implements EventService {
     public EventFullDto getEvent(Long id, HttpServletRequest request) {
         Event event = eventRepository.findByIdAndPublishedOnIsNotNull(id)
                 .orElseThrow(() -> new EventNotExistException(String.format("Can't find event with id = %s event doesn't exist", id)));
-        setView(event);
-        sendStat(event, request);
+        statisticsService.setView(event);
+        statisticsService.sendStat(event, request);
         return eventMapper.toEventFullDto(event);
     }
-    public void sendStat(Event event, HttpServletRequest request) {
-        LocalDateTime now = LocalDateTime.now();
-        String remoteAddr = request.getRemoteAddr();
-        String nameService = "main-service";
 
-        EndpointHitDto requestDto = new EndpointHitDto();
-        requestDto.setTimestamp(now.format(dateFormatter));
-        requestDto.setUri("/events");
-        requestDto.setApp(nameService);
-        requestDto.setIp(remoteAddr);
-        statClient.addStats(requestDto);
-        sendStatForTheEvent(event.getId(), remoteAddr, now, nameService);
-    }
-
-    public void sendStat(List<Event> events, HttpServletRequest request) {
-        LocalDateTime now = LocalDateTime.now();
-        String remoteAddr = request.getRemoteAddr();
-        String nameService = "main-service";
-
-        EndpointHitDto requestDto = new EndpointHitDto();
-        requestDto.setTimestamp(now.format(dateFormatter));
-        requestDto.setUri("/events");
-        requestDto.setApp(nameService);
-        requestDto.setIp(request.getRemoteAddr());
-        statClient.addStats(requestDto);
-        sendStatForEveryEvent(events, remoteAddr, LocalDateTime.now(), nameService);
-    }
-
-    private void sendStatForTheEvent(Long eventId, String remoteAddr, LocalDateTime now,
-                                     String nameService) {
-        EndpointHitDto requestDto = new EndpointHitDto();
-        requestDto.setTimestamp(now.format(dateFormatter));
-        requestDto.setUri("/events/" + eventId);
-        requestDto.setApp(nameService);
-        requestDto.setIp(remoteAddr);
-        statClient.addStats(requestDto);
-    }
-
-    private void sendStatForEveryEvent(List<Event> events, String remoteAddr, LocalDateTime now,
-                                       String nameService) {
-        for (Event event : events) {
-            EndpointHitDto requestDto = new EndpointHitDto();
-            requestDto.setTimestamp(now.format(dateFormatter));
-            requestDto.setUri("/events/" + event.getId());
-            requestDto.setApp(nameService);
-            requestDto.setIp(remoteAddr);
-            statClient.addStats(requestDto);
-        }
-    }
-
+    @Override
     public void setView(List<Event> events) {
         LocalDateTime start = events.get(0).getCreatedOn();
         List<String> uris = new ArrayList<>();
@@ -411,25 +361,8 @@ public class EventServiceImpl implements EventService {
         String startTime = start.format(DateTimeFormatter.ofPattern(DATE));
         String endTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE));
 
-        List<ViewStatsDto> stats = getStats(startTime, endTime, uris);
+        List<ViewStatsDto> stats = statisticsService.getStats(startTime, endTime, uris);
         stats.forEach((stat) ->
                 eventsUri.get(stat.getUri()).setViews(stat.getHits()));
-    }
-
-    public void setView(Event event) {
-        String startTime = event.getCreatedOn().format(dateFormatter);
-        String endTime = LocalDateTime.now().format(dateFormatter);
-        List<String> uris = List.of("/events/" + event.getId());
-
-        List<ViewStatsDto> stats = getStats(startTime, endTime, uris);
-        if (stats.size() == 1) {
-            event.setViews(stats.get(0).getHits());
-        } else {
-            event.setViews(0L);
-        }
-    }
-
-    private List<ViewStatsDto> getStats(String startTime, String endTime, List<String> uris) {
-        return statClient.getStats(startTime, endTime, uris, false);
     }
 }
