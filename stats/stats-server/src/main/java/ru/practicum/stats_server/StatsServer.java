cb27e4d16a7539d5ce52ab05ce4300c6c7c1f1ea
Anton <antnbrnv@yandex.ru>
2023-08-28 15:29:14 +0300

    fix:

--- stats/stats-server/src/main/java/ru/practicum/stats_server/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/stats_server/StatsServer.java
@@ -1,6 +1,5 @@
 package ru.practicum.stats_server;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
